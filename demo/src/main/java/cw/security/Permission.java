2c0c8758b1689f34b1fdcfa2cbc88f5afe0bdd5b
Haaji <soldier.mh.2001@gmail.com>
2021-08-22 11:52:56 +0430

    permissions added to roles

--- demo/src/main/java/cw/security/Permission.java
+++ demo/src/main/java/cw/security/Permission.java
@@ -1,12 +1,50 @@
 package cw.security;
 
 public enum Permission {
-
-
+    //entities-permissions:
+        //students-permission:
     STUDENT_READ("student:read"),
-    STUDENT_WRITE("student:write"),
+    STUDENT_CREATE("student:create"),
+    STUDENT_LIST("student:list"),
+    STUDENT_UPDATE("student:update"),
+    STUDENT_DELETE("student:delete"),
+        //instructor-permissions:
+    INSTRUCTOR_LIST("instructor:list"),
+    INSTRUCTOR_CREATE("instructor:create"),
+    INSTRUCTOR_READ("instructor:read"),
+    INSTRUCTOR_UPDATE("instructor:update"),
+    INSTRUCTOR_DELETE("instructor:delete"),
+        //staff-permissions:
+    STAFF_LIST("staff:list"),
+    STAFF_CREATE("staff:create"),
+    STAFF_READ("staff:read"),
+    STAFF_UPDATE("staff:update"),
+    STAFF_DELETE("staff:delete"),
+        //course-permission:
+    COURSE_LIST("course:list"),
+    COURSE_CREATE("course:create"),
     COURSE_READ("course:read"),
-    COURSE_WRITE("course:write");
+    COURSE_UPDATE("course:update"),
+    COURSE_DELETE("course:delete"),
+        //section-permissions:
+    SECTION_LIST("section:list"),
+    SECTION_CREATE("section:create"),
+    SECTION_READ("section:read"),
+    SECTION_UPDATE("section:update"),
+    SECTION_DELETE("section:delete"),
+        //sectionRegistration-permission:
+    SECTION_REGISTRATION_LIST("sectionRegistration:list"),
+    SECTION_REGISTRATION_CREATE("sectionRegistration:create"),
+    SECTION_REGISTRATION_READ("sectionRegistration:read"),
+    SECTION_REGISTRATION_UPDATE("sectionRegistration:update"),
+    SECTION_REGISTRATION_DELETE("sectionRegistration:delete"),
+        //term-permissions:
+    TERM_LIST("term:list"),
+    TERM_CREATE("term:create"),
+    TERM_READ("term:read"),
+    TERM_UPDATE("term:update"),
+    TERM_DELETE("term:delete");
+
 
     private final String permission;
 
--- demo/src/main/java/cw/security/UserRoles.java
+++ demo/src/main/java/cw/security/UserRoles.java
@@ -1,4 +1,5 @@
 package cw.security;
+
 import com.google.common.collect.Sets;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -6,16 +7,48 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static cw.security.Permission.*;
+
 public enum UserRoles {
-    STUDENT(Sets.newHashSet()),
-    INSTRUCTOR(Sets.newHashSet()),
-    STAFF(Sets.newHashSet());
+    STUDENT(Sets.newHashSet(STUDENT_READ, STUDENT_LIST,
+            TERM_LIST, TERM_READ,
+            INSTRUCTOR_LIST, INSTRUCTOR_READ,
+            COURSE_LIST, COURSE_READ,
+            SECTION_LIST, SECTION_READ)),
+
+
+    INSTRUCTOR(Sets.newHashSet(STUDENT_READ, STUDENT_LIST,
+            TERM_LIST, TERM_READ,
+            INSTRUCTOR_LIST, INSTRUCTOR_READ,
+            COURSE_LIST, COURSE_READ,
+            SECTION_LIST, SECTION_READ, SECTION_CREATE, SECTION_DELETE, SECTION_UPDATE)),
+
+
+    STAFF(Sets.newHashSet(TERM_LIST, TERM_CREATE, TERM_READ, TERM_DELETE, TERM_UPDATE,
+            INSTRUCTOR_LIST, INSTRUCTOR_CREATE, INSTRUCTOR_READ, INSTRUCTOR_UPDATE, INSTRUCTOR_DELETE,
+            STUDENT_READ, STAFF_LIST, STUDENT_CREATE, STUDENT_UPDATE, STAFF_DELETE,
+            COURSE_LIST, COURSE_CREATE, COURSE_READ, COURSE_DELETE, COURSE_UPDATE,
+            SECTION_LIST, SECTION_READ, SECTION_DELETE, SECTION_UPDATE)),
+
+
+    ADMIN(Sets.newHashSet(STUDENT_READ, STAFF_LIST, STUDENT_CREATE, STUDENT_UPDATE, STAFF_DELETE,
+            INSTRUCTOR_LIST, INSTRUCTOR_CREATE, INSTRUCTOR_READ, INSTRUCTOR_UPDATE, INSTRUCTOR_DELETE,
+            STAFF_LIST, STAFF_CREATE, STAFF_READ, STAFF_UPDATE, STAFF_DELETE,
+            COURSE_LIST, COURSE_CREATE, COURSE_READ, COURSE_DELETE, COURSE_UPDATE,
+            SECTION_LIST, SECTION_CREATE, SECTION_READ, SECTION_DELETE, SECTION_UPDATE,
+            TERM_LIST, TERM_CREATE, TERM_READ, TERM_DELETE, TERM_UPDATE));
+
+
+    // SECTION_REGISTRATION_LIST, SECTION_REGISTRATION_CREATE, SECTION_REGISTRATION_READ, SECTION_REGISTRATION_UPDATE,
+    // SECTION_REGISTRATION_DELETE,
+
     @Getter
     private final Set<Permission> permissions;
 
     UserRoles(Set<Permission> permissions) {
         this.permissions = permissions;
     }
+
     public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
         Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
                 .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
